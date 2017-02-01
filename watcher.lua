4926fa16cecff47ed0269a58997b4ac6593b34c9
Kyle Montag <kmontag@sessionm.com>
2017-02-01 09:57:27 -0500

    update watcher to only listen to changes on lua files

--- watcher.lua
+++ watcher.lua
@@ -4,5 +4,5 @@
 function reload_config(files)
   hs.reload()
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/*.lua", reload_config):start()
 hs.alert.show("Config loaded")
